4095d114345e3885aa58702ff781cef4c5e0d367
terryfri3nds <federico@fri3nds.com>
2023-01-02 11:58:29 -0300

    add connecto to api cms

--- builder.js
+++ builder.js
@@ -23,6 +23,6 @@ require("esbuild").buildSync({
   minify: true,
   sourcemap: true,
   outExtension: { ".js": ".min.js", ".css": ".min.css" },
-  target: ["chrome58", "firefox57", "safari11", "edge16"],
+ //target: ["chrome58", "firefox57", "safari11", "edge16"],
   outdir: "dist",
 });
